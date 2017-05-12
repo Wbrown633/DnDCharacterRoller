bad56f0596c05434eca9c6e9cc00b55762a34f64
Will Brown <brown.wi@husky.neu>
2017-05-11 23:00:26 -0400

    Separated Tests from the main file

--- DnDCharacter.py
+++ DnDCharacter.py
@@ -35,21 +35,3 @@ def CompileStats():
     Stats = dict(zip(LISTOFSTATS, List_of_rolls))
     return Stats
 
-class MyTest(unittest.TestCase):
-    def test_ChooseRace(self):
-        self.assertIn(ChooseRace(), RACES, "ERROR: Invalid Race")
-
-    def test_ChooseClass(self):
-        self.assertIn(ChooseClass(), CLASSES, "ERROR: Invalid Class")
-
-    def test_RollSingleStat(self):
-        self.assertIn(RollSingleStat(), range(3, 18), "ERROR: Invalid Roll")
-
-    def test_RolledStats(self):
-        for k, v in CompileStats().items():
-            self.assertIn(k, LISTOFSTATS, "ERROR: Invalid Stat Type Entry")
-            self.assertIn(v, range(21), "ERROR: Invalid Stat Value Entry ")
-
-if __name__ == '__main__':
-    unittest.main()
-
